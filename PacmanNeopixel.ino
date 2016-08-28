da433b8aa7fa249a72daf789183729f3d672a25d
KokoDoko <benni_jolink@hotmail.com>
2016-08-28 18:44:39 +0200

    Pacman can chase ghosts too

--- PacmanNeopixel.ino
+++ PacmanNeopixel.ino
@@ -11,7 +11,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(LAMPS, PIN, NEO_GRB + NEO_KHZ800);
 // (15 leds * 33mA = 495 mA)
 void setup() {
   strip.begin();
-  strip.setBrightness(80);    // overall brightness can be 0 to 255
+  strip.setBrightness(120);    // overall brightness can be 0 to 255
   strip.show();               // Initialize all pixels to 'off'
 }
 
@@ -22,19 +22,23 @@ void loop() {
   
   // pacman eats the food
   eatFood();
-  delay(1800);
+  delay(1500);
 
   // pacman is chased by ghosts
   chasedByGhosts();
-  delay(1800);
+  delay(1500);
+
+  // now pacman is chasing the ghosts
+  chaseGhosts();
+  delay(1500);
 }
 
 // the max brightness here can also be reduced to avoid using too much amps
 void setFood() {
   // fade in
-  for(int br=0; br<200; br++) {
+  for(int br=0; br<170; br++) {
     uint32_t white = strip.Color(br, br, br);
-    for(int i=0; i<strip.numPixels(); i+=2) {
+    for(int i=0; i<strip.numPixels(); i+=4) {
         strip.setPixelColor(i, white); 
     }
     strip.show();
@@ -54,33 +58,49 @@ void eatFood(){
       }
       
       strip.show();
-      delay(100);
+      delay(60);
   }
   strip.setPixelColor(strip.numPixels()-1, off);
   strip.show();
-  delay(100);
+  delay(60);
 }
 
 void chasedByGhosts() {
-  uint32_t yellow = strip.Color(255, 255, 0);
+  uint32_t pacman = strip.Color(255, 255, 0);
   uint32_t clyde = strip.Color(249, 154, 0);
   uint32_t blinky = strip.Color(244, 7, 8);
   uint32_t pinky = strip.Color(249, 161, 166);
   uint32_t inky = strip.Color(48, 218, 219);
 
   // run back
-  for(int i=strip.numPixels(); i>-14; i--) {
-      // pacman - i is position, 0 is space between pacman and ghost (increases per ghost)
-      checkLed(yellow, i, 0);
+  for(int i=strip.numPixels(); i>-30; i--) {
+      // check: color, position, space between pacman and ghost
+      checkLed(pacman, i, 0);
 
-      checkLed(yellow, i, 0);
-      checkLed(clyde, i, 6);
-      checkLed(blinky, i, 8);
-      checkLed(pinky, i, 10);
-      checkLed(inky, i, 12);
+      checkLed(clyde, i, 10);
+      checkLed(blinky, i, 16);
+      checkLed(pinky, i, 22);
+      checkLed(inky, i, 28);
 
       strip.show();
-      delay(100);
+      delay(60);
+  }
+}
+
+void chaseGhosts() {
+  uint32_t pacman = strip.Color(255, 255, 0);
+  uint32_t scaredghost = strip.Color(0, 0, 246);
+
+  // run ghost, run!
+  int maximum = strip.numPixels() + 5;
+  for(int j = -30; j<maximum; j++) {
+      checkLed(scaredghost, j, 28);
+      checkLed(scaredghost, j, 22);
+      checkLed(scaredghost, j, 16);
+      checkLed(scaredghost, j, 10);
+      checkLed(pacman, j, 0);
+      strip.show();
+      delay(60);
   }
 }
 
@@ -91,8 +111,11 @@ void checkLed(uint32_t color, int i, int offset) {
   if(pos < strip.numPixels() && pos > -1){
      strip.setPixelColor(pos, color); 
   }
-  // opvolgende uitzetten
+  // switch off the pixel before and after this one (depends on direction)
   if(pos+1 < strip.numPixels() && pos+1 > -1){
      strip.setPixelColor(pos+1, strip.Color(0, 0, 0));
+  } 
+  if(pos-1 < strip.numPixels() && pos-1 > -1){
+    strip.setPixelColor(pos-1, strip.Color(0, 0, 0));
   } 
 }
